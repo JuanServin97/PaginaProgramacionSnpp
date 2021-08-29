c0f1e9c7da125f8e0a4d7597bb2b35fe02492921
Robert <treboryuzaky@gmail.com>
2021-08-29 15:38:17 -0400

    BK de SNPP

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+//Diccionario de links
+var diccio = {"LenguajeC_Guarani": "https://chat.whatsapp.com/DXihAxihKgDBGXufJnUojT", "ArqPcSo": "https://chat.whatsapp.com/KuMn812LoGsFOVwe6kBZLm", "PBD": "https://chat.whatsapp.com/KNhx4nrpL6s09tUDwpjrOP", "classArqSO": "https://classroom.google.com/c/MzcxOTE5NzcwNjIw?cjc=cdo4b4v", "classCG": "https://classroom.google.com/c/MzcyNTEwMTkzODIw?hl=es&cjc=nnbh6xt", "classPBD": "https://classroom.google.com/c/MzcxOTE5NzcwNjMx?cjc=zd73llr", "classEstadistica": "https://classroom.google.com/c/Mzc2OTA1MzU1NTg4?cjc=4gbe42e"};
+
+//abrir enlaces de whatsapp
+function enlaces(clave) {
+    if (diccio[clave] == "vacio") { //si la clave del diccionario dice vacía, entonces avisa que el enlace no está disponible
+        alert("Este enlace no está disponible");
+        return;
+    }
+    window.open(diccio[clave], '_blank'); //abre el enlace
+    return;
+}
+
+//copiar codigos de classroom
+function copiar(idd){
+    //agrego un campo de texto oculto
+    var cp = document.createElement("input");
+    //asigno el valor que necesito al campo de texto
+    cp.setAttribute("value", document.getElementById(idd).title);
+    //agrego el campo a la página
+    document.body.appendChild(cp);
+    //selecciono el contenido
+    cp.select();
+    //copio el contenido
+    document.execCommand("Copy");
+    //borro el campo de la página
+    document.body.removeChild(cp);
+    //mensaje de copiado
+    alert("Copiado");
+    return;
+}
+
